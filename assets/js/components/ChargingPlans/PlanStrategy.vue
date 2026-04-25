cc260ae13f7f6c7577bf654ca16abfef425ccded
pailakka <peltonen.teemu@gmail.com>
2026-04-25 10:14:11 +0300

    ui: expose precondition strategy controls
    Add advanced charging-plan controls for the new late-window strategy fields. Users can now choose how much late charging contributes to the target and whether the charger should stay available during the late window.
    
    The controls live in the existing strategy panel, preserve backend defaults, and only appear when late charging is enabled. Add English and German labels for the new fields.

--- assets/js/components/ChargingPlans/PlanStrategy.vue
+++ assets/js/components/ChargingPlans/PlanStrategy.vue
@@ -8,7 +8,7 @@
 				</div>
 			</div>
 			<div v-else class="row">
-				<div class="col-12 col-sm-6 col-lg-3 offset-lg-3 mb-3">
+				<div class="col-12 col-sm-6 col-lg-3 mb-3">
 					<div class="row">
 						<label :for="formId('continuous')" class="col-form-label col-5 col-sm-12">
 							{{ $t("main.chargingPlan.optimization.label") }}
@@ -56,6 +56,59 @@
 						</div>
 					</div>
 				</div>
+				<div v-if="preconditionEnabled" class="col-sm-6 col-lg-3 mb-3">
+					<div class="row">
+						<label
+							:for="formId('preconditionContribution')"
+							class="col-form-label col-5 col-sm-12"
+						>
+							{{ $t("main.chargingPlan.preconditionContribution.label") }}
+						</label>
+						<div class="col-7 col-sm-12">
+							<select
+								:id="formId('preconditionContribution')"
+								v-model="localPreconditionContribution"
+								class="form-select"
+								@change="updateStrategy"
+							>
+								<option
+									v-for="opt in preconditionContributionOptions"
+									:key="opt.value"
+									:value="opt.value"
+								>
+									{{ opt.name }}
+								</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<div v-if="preconditionEnabled" class="col-sm-6 col-lg-3 mb-3">
+					<div class="row">
+						<label
+							:for="formId('preconditionSupportMode')"
+							class="col-form-label col-5 col-sm-12"
+						>
+							{{ $t("main.chargingPlan.preconditionSupport.label") }}
+						</label>
+						<div class="col-7 col-sm-12">
+							<select
+								:id="formId('preconditionSupportMode')"
+								v-model="localPreconditionSupportMode"
+								class="form-select"
+								@change="updateStrategy"
+							>
+								<option value="">
+									{{ $t("main.chargingPlan.preconditionSupport.optionNo") }}
+								</option>
+								<option value="keepalive">
+									{{
+										$t("main.chargingPlan.preconditionSupport.optionKeepAlive")
+									}}
+								</option>
+							</select>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -65,6 +118,9 @@
 import { defineComponent, type PropType } from "vue";
 import formatter from "@/mixins/formatter";
 import type { PlanStrategy } from "./types";
+
+const normalizedContribution = (value?: number) =>
+	value === undefined || Number.isNaN(value) ? 1 : value;
 
 export default defineComponent({
 	name: "ChargingPlanStrategy",
@@ -82,9 +138,16 @@ export default defineComponent({
 		return {
 			localPrecondition: this.precondition,
 			localContinuous: this.continuous,
+			localPreconditionContribution: normalizedContribution(
+				this.strategy?.preconditionContribution
+			),
+			localPreconditionSupportMode: this.strategy?.preconditionSupportMode || "",
 		};
 	},
 	computed: {
+		preconditionEnabled() {
+			return this.localPrecondition > 0;
+		},
 		preconditionOptions() {
 			const HOUR = 60 * 60;
 			const QUARTER_HOUR = 0.25 * HOUR;
@@ -108,6 +171,14 @@ export default defineComponent({
 						: this.fmtDurationLong(s),
 			}));
 		},
+		preconditionContributionOptions() {
+			return [0, 0.25, 0.5, 0.75, 1].map((value) => ({
+				value,
+				name: this.$t("main.chargingPlan.preconditionContribution.option", {
+					value: this.fmtPercentage(value * 100),
+				}),
+			}));
+		},
 	},
 	watch: {
 		precondition: {
@@ -128,6 +199,21 @@ export default defineComponent({
 			},
 			immediate: true,
 		},
+		strategy: {
+			deep: true,
+			handler(newValue: PlanStrategy | undefined) {
+				const contribution = normalizedContribution(newValue?.preconditionContribution);
+				if (contribution !== this.localPreconditionContribution) {
+					this.localPreconditionContribution = contribution;
+				}
+
+				const supportMode = newValue?.preconditionSupportMode || "";
+				if (supportMode !== this.localPreconditionSupportMode) {
+					this.localPreconditionSupportMode = supportMode;
+				}
+			},
+			immediate: true,
+		},
 	},
 	methods: {
 		formId(name: string) {
@@ -138,6 +224,9 @@ export default defineComponent({
 				...(this.strategy || {}),
 				continuous: this.localContinuous,
 				precondition: this.localPrecondition,
+				preconditionContribution: this.localPreconditionContribution,
+				preconditionSupportMode: this
+					.localPreconditionSupportMode as PlanStrategy["preconditionSupportMode"],
 			};
 			this.$emit("update", strategy);
 		},
